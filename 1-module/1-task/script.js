7e933dcd8cfc2e203034f58c269f812e22abc12d
Daria <d.gritsaenko@articul.ru>
2020-06-28 19:41:32 +0300

    увеличение числа на 1 при клике на кнопку

--- 1-module/1-task/script.js
+++ 1-module/1-task/script.js
@@ -2,3 +2,18 @@ import Vue from '/vendor/vue.esm.browser.js';
 
 // const app = ...
 // Рекомендуется использовать МЕТОД в качестве обработчика события
+const app = new Vue({
+	el: '#app',
+
+	data: {
+		counter: 0
+	},
+
+	methods: {
+		increaseValue() {
+			this.counter += 1;
+		}
+	}
+});
+
+window.app = app;
